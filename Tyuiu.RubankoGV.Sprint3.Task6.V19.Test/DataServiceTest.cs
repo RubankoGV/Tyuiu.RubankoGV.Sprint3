13972dd949552a4f8b9e284692e705b3760e72f8
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-14 19:02:32 +0300

    Task6

---
+++ Tyuiu.RubankoGV.Sprint3.Task6.V19.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.RubankoGV.Sprint3.Task6.V19.Lib;
+namespace Tyuiu.RubankoGV.Sprint3.Task6.V19.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Tyuiu.RubankoGV.Sprint3.Task6.V19/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.RubankoGV.Sprint3.Task6.V19.Lib;
+namespace Tyuiu.RubankoGV.Sprint3.Task6.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [10, 15] количество всех делителей меньше 8           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 10;
+            int stopValue = 15;
+
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
