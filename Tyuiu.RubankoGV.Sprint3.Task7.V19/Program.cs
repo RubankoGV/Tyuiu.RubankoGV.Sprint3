1a3976def13ab4fe5db7bf81d0f64e4e31fb5ea0
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-14 19:06:40 +0300

    Task7

---
+++ Tyuiu.RubankoGV.Sprint3.Task7.V19/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.RubankoGV.Sprint3.Task7.V19.Lib;
+namespace Tyuiu.RubankoGV.Sprint3.Task7.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу занчений функции            *");
+            Console.WriteLine("* F(x) = (5x + 2,5)/(sin(x) - 2) + 2                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага - " + startValue);
+            Console.WriteLine("Конец шага - " + stopValue);
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив значений функции - " + "[{0}]", string.Join(",", valueArray));
+            Console.ReadKey();
+        }
+    }
+}
